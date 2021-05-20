02153f61a03a4efcbe2b52af9a1816e519883f70
Hien (Relia Hackintosh) <hienlh1298@gmail.com>
2021-05-20 17:11:00 +0700

    chore: test send email

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import { getInput, setOutput, setFailed } from '@actions/core';
-import { context } from '@actions/github';
+'use strict'
 
-try {
-    const email = getInput('email');
-    console.log(`Hello ${email}!`);
-    setOutput("message", 'Success');
-    const payload = JSON.stringify(context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    setFailed(error.message);
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+const main = async () => {
+    try {
+        const email = core.getInput('email') || 'hienlh1298@gmail.com';
+        console.log(`Hello ${email}!`);
+
+        const res = await axios.post('https://simple-send-mail-service.herokuapp.com/send-mail', {
+            email: email,
+            title: 'Test email',
+            body: 'Test email'
+        });
+
+        if (res.statusCode >= 400) {
+            throw res.statusText
+        }
+
+        core.setOutput("message", 'Success');
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error);
+    }
 }
+
+main();
